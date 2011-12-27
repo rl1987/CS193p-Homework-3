0a002886314f0c0557f63bc35bb7014671142b79
R L <rl1987@sdf.lonestar.org>
2011-12-27 14:32:18 +0200

    Initial Commit

---
+++ GraphCalc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GraphCalc
+//
+//  Created by avitela avitela on 2011-12-27.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
